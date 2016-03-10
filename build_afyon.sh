1cdba6fe58fe2c91bf4c5db051d9eda536a04fe8
derek <dm47021@hotmail.com>
2016-03-10 09:48:36 -0500

    Initial Samsung Kernel Source: - Fixed Radio-Iris Errors - Fixed Usb Gadget Errors

---
+++ build_afyon.sh
@@ -0,0 +1,3 @@
+
+make -j64 ARCH=arm CROSS_COMPILE=/home/dm47021/Android/toolchains/arm-eabi-4.7/bin/arm-eabi- msm8226-sec_defconfig VARIANT_DEFCONFIG=msm8926-sec_afyonltetmo_defconfig
+make -j64 ARCH=arm CROSS_COMPILE=/home/dm47021/Android/toolchains/arm-eabi-4.7/bin/arm-eabi-
