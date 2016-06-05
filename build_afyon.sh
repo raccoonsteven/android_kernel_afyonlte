60e239cfab4969250cf983c39863e35d60d1a86e
derek <dm47021@hotmail.com>
2016-06-05 12:20:33 -0400

    Zero: Add Support for AOKP Roms

--- build_afyon.sh
+++ build_afyon.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 
-export version=ZERO_KERNEL-0.3d-ANK3
+export version=ZERO_KERNEL-0.3d-AOKP
 export CROSS_COMPILE=/home/dm47021/Android/toolchains/arm-eabi-4.7/bin/arm-eabi-
 
 # Colorize and add text parameters
@@ -88,7 +88,7 @@ echo -e "${bldcya}
 ${txtrst}" 
 
 cd zfiles
-./mkbootzip.pl ramdisk/zero
+./mkbootzip.pl ramdisk/zero_aokp
 
 echo""
 echo -e "${bldcya} 
