0955178add3ee0e341b6bd9a23c4069c17e1033d
derek <dm47021@hotmail.com>
2016-06-11 17:12:14 -0400

    REAPER: Cleanup, rebase,add touch boost interface

--- include/trace/events/cpufreq_reaper.h
+++ include/trace/events/cpufreq_reaper.h
@@ -28,7 +28,13 @@ DECLARE_EVENT_CLASS(set,
 	      __entry->actualfreq)
 );
 
-DEFINE_EVENT(set, cpufreq_reaper_setspeed,
+DEFINE_EVENT(set, cpufreq_reaper_up,
+	TP_PROTO(u32 cpu_id, unsigned long targfreq,
+	     unsigned long actualfreq),
+	TP_ARGS(cpu_id, targfreq, actualfreq)
+);
+
+DEFINE_EVENT(set, cpufreq_reaper_down,
 	TP_PROTO(u32 cpu_id, unsigned long targfreq,
 	     unsigned long actualfreq),
 	TP_ARGS(cpu_id, targfreq, actualfreq)
@@ -36,88 +42,44 @@ DEFINE_EVENT(set, cpufreq_reaper_setspeed,
 
 DECLARE_EVENT_CLASS(loadeval,
 	    TP_PROTO(unsigned long cpu_id, unsigned long load,
-		     unsigned long curtarg, unsigned long curactual,
-		     unsigned long newtarg),
-		    TP_ARGS(cpu_id, load, curtarg, curactual, newtarg),
+		     unsigned long curfreq, unsigned long targfreq),
+	    TP_ARGS(cpu_id, load, curfreq, targfreq),
 
 	    TP_STRUCT__entry(
 		    __field(unsigned long, cpu_id    )
 		    __field(unsigned long, load      )
-		    __field(unsigned long, curtarg   )
-		    __field(unsigned long, curactual )
-		    __field(unsigned long, newtarg   )
+		    __field(unsigned long, curfreq   )
+		    __field(unsigned long, targfreq  )
 	    ),
 
 	    TP_fast_assign(
 		    __entry->cpu_id = cpu_id;
 		    __entry->load = load;
-		    __entry->curtarg = curtarg;
-		    __entry->curactual = curactual;
-		    __entry->newtarg = newtarg;
+		    __entry->curfreq = curfreq;
+		    __entry->targfreq = targfreq;
 	    ),
 
-	    TP_printk("cpu=%lu load=%lu cur=%lu actual=%lu targ=%lu",
-		      __entry->cpu_id, __entry->load, __entry->curtarg,
-		      __entry->curactual, __entry->newtarg)
+	    TP_printk("cpu=%lu load=%lu cur=%lu targ=%lu",
+		      __entry->cpu_id, __entry->load, __entry->curfreq,
+		      __entry->targfreq)
 );
 
 DEFINE_EVENT(loadeval, cpufreq_reaper_target,
 	    TP_PROTO(unsigned long cpu_id, unsigned long load,
-		     unsigned long curtarg, unsigned long curactual,
-		     unsigned long newtarg),
-	    TP_ARGS(cpu_id, load, curtarg, curactual, newtarg)
+		     unsigned long curfreq, unsigned long targfreq),
+	    TP_ARGS(cpu_id, load, curfreq, targfreq)
 );
 
 DEFINE_EVENT(loadeval, cpufreq_reaper_already,
 	    TP_PROTO(unsigned long cpu_id, unsigned long load,
-		     unsigned long curtarg, unsigned long curactual,
-		     unsigned long newtarg),
-	    TP_ARGS(cpu_id, load, curtarg, curactual, newtarg)
+		     unsigned long curfreq, unsigned long targfreq),
+	    TP_ARGS(cpu_id, load, curfreq, targfreq)
 );
 
 DEFINE_EVENT(loadeval, cpufreq_reaper_notyet,
 	    TP_PROTO(unsigned long cpu_id, unsigned long load,
-		     unsigned long curtarg, unsigned long curactual,
-		     unsigned long newtarg),
-	    TP_ARGS(cpu_id, load, curtarg, curactual, newtarg)
-);
-
-DECLARE_EVENT_CLASS(modeeval,
-	    TP_PROTO(unsigned long cpu_id, unsigned long total_load,
-		     unsigned long single_enter, unsigned long multi_enter,
-		     unsigned long single_exit, unsigned long multi_exit, unsigned long mode),
-		    TP_ARGS(cpu_id, total_load, single_enter, multi_enter, single_exit, multi_exit, mode),
-
-	    TP_STRUCT__entry(
-		    __field(unsigned long, cpu_id      )
-		    __field(unsigned long, total_load  )
-		    __field(unsigned long, single_enter)
-		    __field(unsigned long, multi_enter )
-		    __field(unsigned long, single_exit )
-		    __field(unsigned long, multi_exit  )
-		    __field(unsigned long, mode)
-	    ),
-
-	    TP_fast_assign(
-		    __entry->cpu_id = cpu_id;
-		    __entry->total_load = total_load;
-		    __entry->single_enter = single_enter;
-		    __entry->multi_enter = multi_enter;
-		    __entry->single_exit = single_exit;
-		    __entry->multi_exit = multi_exit;
-		    __entry->mode = mode ;
-	    ),
-
-	    TP_printk("cpu=%lu load=%3lu s_en=%6lu m_en=%6lu s_ex=%6lu m_ex=%6lu ret=%lu",
-		      __entry->cpu_id, __entry->total_load, __entry->single_enter,
-		      __entry->multi_enter, __entry->single_exit, __entry->multi_exit, __entry->mode)
-);
-
-DEFINE_EVENT(modeeval, cpufreq_reaper_mode,
-	    TP_PROTO(unsigned long cpu_id, unsigned long total_load,
-		     unsigned long single_enter, unsigned long multi_enter,
-		     unsigned long single_exit, unsigned long multi_exit, unsigned long mode),
-	    TP_ARGS(cpu_id, total_load, single_enter, multi_enter, single_exit, multi_exit, mode)
+		     unsigned long curfreq, unsigned long targfreq),
+	    TP_ARGS(cpu_id, load, curfreq, targfreq)
 );
 
 TRACE_EVENT(cpufreq_reaper_boost,
